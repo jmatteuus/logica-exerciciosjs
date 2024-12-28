8093c8bbd9588375238933f579681dd77385e96d
João Mateus <joaomateuscvieira@gmail.com>
2024-12-28 14:48:57 -0300

    exercicio - frequencia de números aleatorios

--- exercicios(parte2)/script.js
+++ exercicios(parte2)/script.js
@@ -9,3 +9,19 @@ console.log(anagrama('estudo','duetos'));
 
 
 //Exercicio 02
+let numeros = [];
+let frequencia = {};
+for(let i = 1; i <= 100; i++){
+    numeros[i] = Math.floor(Math.random() * 6) + 1;
+}
+numeros.forEach(n => {
+    frequencia[n] = (frequencia[n] || 0) + 1;
+});
+function resultado() {
+    console.log('A quantidade de cada número é:');
+    for (let num in frequencia) {
+      console.log(`Número ${num}: ${frequencia[num]} vezes`);
+    }
+  }
+  
+resultado();
