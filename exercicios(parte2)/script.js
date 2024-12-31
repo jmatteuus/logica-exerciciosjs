b2eb397f39a281d247c0972c4d3dba9ef5ec86d4
João Mateus <joaomateuscvieira@gmail.com>
2024-12-31 13:48:19 -0300

    Update script.js

--- exercicios(parte2)/script.js
+++ exercicios(parte2)/script.js
@@ -1,37 +1,37 @@
 //Exercicio 01 (Anagrama)
 const anagrama = (frase1, frase2) => {
-    const Frase1 = frase1.split('').sort().join('');
-    const Frase2 = frase2.split('').sort().join('');
+  const Frase1 = frase1.split('').sort().join('');
+  const Frase2 = frase2.split('').sort().join('');
 
-    return Frase1 === Frase2;
+  return Frase1 === Frase2;
 };
-console.log(anagrama('estudo','duetos'));
+console.log(anagrama('estudo', 'duetos'));
 
 
 //Exercicio 02
 let numeros = [];
 let frequencia = {};
-for(let i = 1; i <= 100; i++){
-    numeros[i] = Math.floor(Math.random() * 6) + 1;
+for (let i = 1; i <= 100; i++) {
+  numeros[i] = Math.floor(Math.random() * 6) + 1;
 }
 numeros.forEach(n => {
-    frequencia[n] = (frequencia[n] || 0) + 1;
+  frequencia[n] = (frequencia[n] || 0) + 1;
 });
 function resultado() {
-    console.log('A quantidade de cada número é:');
-    for (let num in frequencia) {
-      console.log(`Número ${num}: ${frequencia[num]} vezes`);
-    }
+  console.log('A quantidade de cada número é:');
+  for (let num in frequencia) {
+    console.log(`Número ${num}: ${frequencia[num]} vezes`);
   }
+}
 resultado();
 
 
 //Exercicio 03
 const pessoas = [
-  {nome: 'Arthur', idade: 25},
-  {nome: 'Caio', idade: 24},
-  {nome: 'Levi', idade: 35},
-  {nome: 'Mateus', idade: 21}
+  { nome: 'Arthur', idade: 25 },
+  { nome: 'Caio', idade: 24 },
+  { nome: 'Levi', idade: 35 },
+  { nome: 'Mateus', idade: 21 }
 ]
 
 function ordenarPorPropriedade(array, propriedade) {
@@ -49,9 +49,9 @@ console.log(ordenarPorPropriedade(pessoas, 'nome'));
 
 
 //Exercicio 04
-const num = [5,5,6,7,9,7,7,9,4,2,3,5,1];
+const num = [5, 5, 6, 7, 9, 7, 7, 9, 4, 2, 3, 5, 1];
 
-function obterNumerosUnicos(array){
+function obterNumerosUnicos(array) {
   let numerosUnicos = array.filter((num, index, array) => {
     return array.indexOf(num) === array.lastIndexOf(num);
   });
@@ -61,10 +61,10 @@ console.log(obterNumerosUnicos(num));
 
 
 //Exercicio 05
-function calcularTempoExecucao(funcao){
+function calcularTempoExecucao(funcao) {
 
   const inicio = performance.now();
-  for(let i = 0; i < 1000000; i++){
+  for (let i = 0; i < 1000000; i++) {
     funcao()
   }
   const fim = performance.now();
@@ -79,11 +79,11 @@ console.log(calcularTempoExecucao(exemplo).toFixed(2));
 
 
 //Exercicio 06
-function fibonacci(){
+function fibonacci() {
   let n1 = 0;
   let n2 = 1;
   let sequencia = `${n1} ${n2} `;
-  for(let i = 0;i <= 10;i++){
+  for (let i = 0; i <= 10; i++) {
     let n3 = n1 + n2;
     sequencia += `${n3} `
     n1 = n2;
@@ -95,5 +95,11 @@ fibonacci();
 
 
 //Exercicio 07
-const Num = [-6,-3,9,5,-7,10,-15,4,7,3];
+const Num = [-6, -3, 9, 5, -7, 10, -15, 4, 7, 3];
+let positivo = Num.filter(num => num > 0);
+console.log(positivo);
+let negativo = Num.filter(num => num < 0);
+console.log(negativo);
 
+
+//Exercicio 08
